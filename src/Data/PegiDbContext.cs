29a8527c6b60d51623d5c100e0edd002be8620d0
Santiago <sadavid@unicesar.edu.co>
2022-10-06 16:35:23 -0500

    add class UserService, repository, PegiDbContext

---
+++ src/Data/PegiDbContext.cs
@@ -0,0 +1,18 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class PegiDbContext : DbContext
+{
+    public PegiDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasIndex(user => user.Name).IsUnique();
+    }
+}
--- src/Entities/User.cs
+++ src/Entities/User.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Entities;
 
 [Table("users")]
-public class user
+public class User
 {
     [Key]
     [Column("user_name")]

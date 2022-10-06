b6f8a4d7cf686f68edb08f012d5ef53820ad0883
Santiago <sadavid@unicesar.edu.co>
2022-10-06 16:15:53 -0500

    add class user

---
+++ src/Entities/user.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities;
+
+[Table("users")]
+public class user
+{
+    [Key]
+    [Column("user_name")]
+    public string Name { get; set; }
+
+    [Column("user_password")]
+    public string Password { get; set; }
+
+    [Column("user_rol")]
+    public string Role { get; set; }
+}
